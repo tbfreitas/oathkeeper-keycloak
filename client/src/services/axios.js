4eca7a76edbb3f8b1c541406959754380e10e46a
tarcisio <tarcisiodf23@gmail.com>
2022-09-02 12:15:14 -0300

    Upload from client dockerfile. Must finish the client with keycloak.js.

--- client/src/services/axios.js
+++ client/src/services/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = {
   api_oauth2: axios.create({
-    baseURL: "http://localhost:8080/auth",
+    baseURL: `${process.env.REACT_APP_KEYCLOAK}/auth`,
   }),
 
   api_spring: axios.create({
